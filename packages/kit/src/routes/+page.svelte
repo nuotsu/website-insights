7f704446c738d2dc89f3c9e25b40e7f55b70a5ce
Mitchell Christ <xzmaru@gmail.com>
2022-09-05 18:07:03 -0700

    test github example

--- packages/kit/src/routes/+page.svelte
+++ packages/kit/src/routes/+page.svelte
@@ -5,6 +5,19 @@
 </section>
 
 <section>
+	<form name="netlify-form-example" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+		<input type="hidden" name="form-name" value="netlify-form-example" />
+		<label for="name">Name</label>
+		<input name="name" id="name" required placeholder="Name" type="text" />
+		<label for="email">Email</label>
+		<input name="email" id="email" required placeholder="Email" type="email" />
+		<label for="message">Message</label>
+		<input name="message" id="message" required placeholder="Message" type="text" />
+		<input type="submit" value="Submit" />
+	</form>
+</section>
+
+<!-- <section>
 	<Form name="review-request">
 		<h2>Get your site reviewed!</h2>
 
@@ -15,7 +28,7 @@
 
 		<button>Submit for review</button>
 	</Form>
-</section>
+</section> -->
 
 <script>
 	import site from '$data/site.js'
